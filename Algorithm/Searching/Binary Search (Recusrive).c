4e9119215da84ab65467a1560828e4619445e108
Soumyajit <soumyajitezio@gmail.com>
2021-06-07 12:04:16 +0530

    Added binary search

---
+++ Algorithm/Searching/Binary Search (Recusrive).c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+
+int binary_search(int A[], int l, int h, int key)
+{
+    if(l == h) //if key is not found even when l and h coincide, then element is not present
+    {
+        if(A[l] == key)
+            return l;
+        else
+            return -1;
+    }
+    else
+    {
+        int mid = l + (h-l)/2;
+
+        if(key == A[mid])
+            return mid;
+
+        if(key < A[mid])
+            return binary_search(A, l, mid-1, key);
+        else
+            return binary_search(A, mid+1, h, key);
+    }
+}
+
+int main()
+{
+    int n, key;
+    printf("Enter no of elements: ");
+    scanf("%d", &n);
+    int A[n];
+
+    printf("Enter array in sorted order: \n");
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d",&A[i]);
+    }
+
+    //assume array to be sorted
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    int result = binary_search(A,0,n-1,key);
+
+    if(result == -1)
+        printf("Element not found");
+    else
+        printf("Element found at location %d", result+1);
+
+    return 0;
+}
